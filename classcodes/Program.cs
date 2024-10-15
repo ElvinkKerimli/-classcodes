617c6813cb86b3c1a1faa448b0e3dea28318222c
Amin <aminib-ab205@code.edu.az>
2024-10-15 09:46:32 +0400

    first

---
+++ classcodes/Program.cs
@@ -0,0 +1,76 @@
+﻿using System.Drawing;
+using System.IO;
+using System.Xml;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student("Elvin", "kerimli", "AB107", 87, true);
+
+            student.GetInfo("Elvin", "kerimli", "AB107", 87, true);
+
+        }
+
+        
+    }
+
+    class Student
+    {
+        public string Name;
+        public string Surname;
+        public string Group;
+        public int Point;
+        public bool Isgraduated;
+
+
+        public Student(string name, string surname, string group, int point, bool isgraduated)
+        {
+            Name = name;
+            Surname = surname;
+            Group = group;
+            Point = point;
+            Isgraduated = isgraduated;
+        }
+        public void GetInfo(string name, string surname, string group, int point, bool isgraduated)
+        {
+            Console.WriteLine(name + " " + surname + " " + group + " " + point + " " + isgraduated);
+
+        }
+        public void CheckGraduation()
+        {
+            if (Isgraduated)
+            {
+                Console.WriteLine("Telebe mezun olub ");
+
+            }
+            else {
+
+                Console.WriteLine("telebe mezun olmayib");
+            }
+        }
+        public void GetDiplomDegree(int point)
+        {
+            if (point < 65)
+            {
+                Console.WriteLine("telebenin diblomu yoxdur");
+            }
+            else if (point < 80)
+            {
+                Console.WriteLine("Telebe adi diplom alib");
+            }
+            else if (point < 90)
+            {
+                Console.WriteLine("telebe seref diplomu alib");
+            }
+            else 
+            {
+                Console.WriteLine("telebe yuksek seref alib");
+            
+            }
+        }
+
+    }
+}
---
+++ classcodes/Program1.cs
@@ -0,0 +1,52 @@
+﻿
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car("mercedes", "sclass", 70, 2, 125000);
+      
+            car.Drive(30);
+        }
+    }
+    class Car
+    {
+        public string Brand;
+        public string Model;
+        public int CurrentFuel;
+        public int FuelFor1km;
+        public int Millage;
+        public int km;
+      
+
+        public Car(string brand, string model, int currentFuel, int fuelFor1km, int millage)
+        {
+            Brand = brand;
+            Model = model;
+            CurrentFuel = currentFuel;
+            FuelFor1km = fuelFor1km;
+            Millage = millage;
+        }
+
+        public void Drive( int km)
+        {
+
+            if (km * FuelFor1km > CurrentFuel)
+            {
+                Console.WriteLine("xəta");
+            }
+            else 
+            {
+                CurrentFuel= CurrentFuel - km*FuelFor1km;
+                Millage=Millage + km;
+                Console.WriteLine(CurrentFuel+" "+Millage);
+                
+            }
+
+        }
+
+        
+    }
+}

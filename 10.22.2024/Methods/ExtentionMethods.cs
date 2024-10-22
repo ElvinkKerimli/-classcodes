be6c200a50fdabbaa5738e486583bb5196e62cde
Amin <aminib-ab205@code.edu.az>
2024-10-22 12:39:03 +0400

    bu unun taski

---
+++ 10.22.2024/Methods/ExtentionMethods.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp24.Methods
+{
+    internal static class ExtentionMethods
+    {
+        public static bool IsOdd(this int value)
+        {
+            if (value % 2 == 1)
+            {
+                return true;
+                Console.WriteLine(true);
+            }
+            else
+            {
+                return false;
+                Console.WriteLine("false");
+            }
+
+        }
+        public static bool IsEven(this int value)
+        {
+            if (value % 2 == 0)
+            {
+                Console.WriteLine("true");
+                return true;
+                
+            }
+            else
+            {
+                return false;
+                Console.WriteLine("false");
+            }
+        }
+        public static string ToCapitalize(this string name)
+        {
+            name = name.ToUpper();
+
+            return name;
+            Console.WriteLine(name);
+        }
+        public static void GetValueIndex(this char i, string name)
+        {
+            for (int j = 0; j < name.Length; j++)
+            {
+                if (i == j)
+                {
+                    Console.WriteLine("daxil etdiyiniz char stringde var");
+
+                }
+                else
+                {
+
+                    Console.WriteLine("daxil etdiyiniz char stringde yoxdur");
+
+                }
+
+            }
+        }
+        public static bool CustomContains(this char name, string cumle)
+        {
+            for (int j = 0; j < cumle.Length; j++)
+            {
+                if (j == name)
+                {
+                    
+                    Console.WriteLine("soz cumlede var");
+                    bool x = true;
+                    return x;
+                }
+                else
+                {
+                    
+                    Console.WriteLine("soz cumlede yoxdur");
+                    bool x= false;
+                    return x;
+
+
+                }
+                return x;
+            }
+            
+        }
+    }
+}
---
+++ 10.22.2024/Program.cs
@@ -0,0 +1,12 @@
+﻿using ConsoleApp24.Methods;
+namespace ConsoleApp24
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Methods.ExtentionMethods.IsOdd(6);
+        }
+    }
+}
